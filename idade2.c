93dcb976d2e80486761b6bc17a5aa3df77c045e9
EdilzaA <edilzaprogramacao.github@gmail.com>
2025-04-02 02:27:15 +0000

    new file:   idade2.c

---
+++ idade2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){ 
+    
+    
+    int idade1 = 25; 
+    int idade2 = 25; 
+
+
+
+
+    if (idade1 = idade2){
+        printf("A pessoa 1 tem a mesma idade da pessoa 2\n"); //Só vai executar aqui dentro o que for verdadeiro, EXECUTOU PA A CONDIÇÃO FOI VERDADEIRA
+        printf("Dentro IF\n");
+
+    }
+
+        printf("Fora IF\n");
+    
+    
+        
+   
+
+}
