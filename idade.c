67c818e84c4b978837b913220cf5177b875af5ed
EdilzaA <edilzaprogramacao.github@gmail.com>
2025-04-02 01:55:49 +0000

    new file:   idade.c

---
+++ idade.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){ 
+    /*Se a idade for 18 oumais, o programa imprime 'Você é maior de idade'.
+    No nosso exemplo, 20 é maior que 18, então a mensagem será exibida*/
+    
+    int idade = 20;
+
+
+
+    if (idade >= 18 )
+        printf("Você é maior de idade\n"); //Só vai executar aquidentro o que for verdadeiro
+        printf("Fora IF");
+    
+       
+        return 0;
+   
+
+}
