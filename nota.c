33dd532b8bf30e664e8772e33671b856ac14deb9
EdilzaA <edilzaprogramacao.github@gmail.com>
2025-04-02 02:16:44 +0000

    new file:   nota.c

---
+++ nota.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){ 
+    
+    
+    int nota = 70; 
+
+
+
+    if (nota >= 60 ){
+        printf("Parabéns!!! você passou\n"); 
+     
+    }
+
+        printf("Fora IF\n");
+    
+    
+        
+   
+
+}
